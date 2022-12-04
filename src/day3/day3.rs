52afa4e9773fbc5d4ec61f78a2fb82223e450c0d
Trevor <trevortomlin2@outlook.com>
2022-12-03 18:15:57 -0800

    Day3 complete.

---
+++ src/day3/day3.rs
@@ -0,0 +1,126 @@
+use std::collections::{HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn run() -> (u32, u32) {
+
+    let file_path = "src/day3/input.txt";
+
+    let file = File::open(file_path).expect("Error in reading file");
+    let lines = BufReader::new(file).lines();
+
+    let mut total: u32 = 0;
+    let mut total2: u32 = 0;
+
+    let mut lineIndex = 0;
+
+    let mut set21: HashSet<char> = HashSet::new();
+    let mut set22: HashSet<char> = HashSet::new();
+    let mut set23: HashSet<char> = HashSet::new();
+
+    for line in lines {
+
+        if let Ok(line) = line {
+
+            let (comp1, comp2) = line.split_at(line.len()/2); 
+
+            let mut set1 = HashSet::new();
+            let mut set2 = HashSet::new();
+
+            match lineIndex % 3 {
+                0 => { 
+
+                    for x in set21.intersection(&set22).find(|it| set23.contains(it)) {
+
+                        let x = *x as u32;
+
+                        if x >= 97 {
+
+                            total2 += x - 96;
+        
+                        }
+        
+                        else {
+        
+                            total2 += x - 38;
+        
+                        }
+
+                        //print!("{}", total2);
+
+                    }
+
+                    //println!();
+
+                    set21.clear();
+                    set22.clear();
+                    set23.clear();
+                                        
+                    set21 = line.chars().collect(); 
+
+                },
+                1 => set22 = line.chars().collect(),
+                2 => set23 = line.chars().collect(),
+                _ => panic!("Error"),
+            };
+
+            for s in comp1.bytes() {
+
+                set1.insert(s);
+
+            }
+
+            for s in comp2.bytes() {
+
+                set2.insert(s);
+
+            }
+
+            let intersection: HashSet<_> = set1.intersection(&set2).collect();
+
+            for x in intersection {
+
+                let x = u32::from(*x);
+
+                // lower case
+                if x >= 97 {
+
+                    total += x - 96;
+
+                }
+
+                else {
+
+                    total += x - 38;
+
+                }
+
+            }
+
+        }
+
+        lineIndex += 1;
+
+    }
+
+    for x in set21.intersection(&set22).find(|it| set23.contains(it)) {
+
+        let x = *x as u32;
+
+        if x >= 97 {
+
+            total2 += x - 96;
+
+        }
+
+        else {
+
+            total2 += x - 38;
+
+        }
+
+    }
+
+    (total, total2)
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let d1 = day1::day1::run();
@@ -7,4 +8,7 @@ fn main() {
     
     let d2 = day2::day2::run();
     println!("Day2 : {}, {}", d2.0, d2.1);
+
+    let d3 = day3::day3::run();
+    println!("Day3 : {}, {}", d3.0, d3.1);
 }
